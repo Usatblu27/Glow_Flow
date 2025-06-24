466f8da31ac72356eb820c7f0c5edb5a860c4770
Usatblu27 <daniilusatenko2772@gmail.com>
2025-06-24 13:18:28 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,23 @@
+// Простейший рабочий вариант
+const CACHE_NAME = 'v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/game.js',
+  '/icon192.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
